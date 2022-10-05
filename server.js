128c1fef144488cc44abfc82cab5117257deffa1
Rachel <marcotter25@yahoo.com>
2022-10-05 16:36:05 -0700

    proof read prompt questions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2');
 
+//create the connection to the database
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -10,19 +11,22 @@ const connection = mysql.createConnection({
     database: 'employee_db'
 });
 
+//start the app
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Welcome to the Employee Manager App!")
     initialQuestion();
 });
 
+//First prompt question
 function initialQuestion() {
     inquirer.prompt(
         {
             type: 'list',
             name: 'initialQuestion',
             message: 'What would you like to do?',
-            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add an employee', 'add a role', 'update an employee role']
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add an employee', 'add a role', 
+            'update an employee role', 'delete employee', 'delete role', 'delete department', 'Quit']
         },
     ).then(function (userInput) {
         switch (userInput.initialQuestion) {
@@ -47,22 +51,33 @@ function initialQuestion() {
             case 'update an employee role':
                 updateRole();
                 break;
+            case 'delete employee':
+                deleteEmployee();
+                break;
+            case 'delete role':
+                deleteRoles();
+                break;
+            case 'delete department':
+                deleteDepartment();
+                break;
             default:
-                alert('You must pick one!');
+                console.log("In order to quit Employee Manager please hit 'Ctrl C' and when prompted enter Y, See you next time!");
                 return;
 
         }
     });
 };
 
+//Show all departments
 function departmentTable() {
     connection.query('SELECT * FROM departments', function (err, results) {
         if (err) throw err;
         console.table(results);
         initialQuestion();
     });
-}
+};
 
+//Show all roles
 function rolesTable() {
     connection.query('SELECT roles.id, title, salary, department_id, department_name FROM roles JOIN departments ON roles.department_id = departments.id', 
     function (err, results) {
@@ -72,9 +87,10 @@ function rolesTable() {
     })
 };
 
+//Show all employees
 function employeesTable() {
     connection.query(
-        'SELECT first_name, last_name, title, department_name, salary, manager FROM roles JOIN employees ON roles.id = employees.role_id JOIN departments ON roles.department_id = departments.id', 
+        'SELECT employees.id, first_name, last_name, title, department_name, salary, manager FROM roles JOIN employees ON roles.id = employees.role_id JOIN departments ON roles.department_id = departments.id', 
         function (err, results) {
         
         if (err) throw err;
@@ -83,6 +99,7 @@ function employeesTable() {
     })
 };
 
+//add a department to department table
 function addDepartment () {
     inquirer.prompt([
         {
@@ -102,14 +119,14 @@ function addDepartment () {
         let sql = 'INSERT INTO departments(department_name) VALUES(?)';
         const department = userInput.newDepartment
 
-        connection.query(sql, department, function (err, results) {
+        connection.query(sql, department, function (err) {
             if (err) throw (err);
             console.table(departmentTable());
         });
     });
 }
 
-//add first_name, last_name, role, and manager
+//Add employee to employees table
 function addEmployee() {
     inquirer.prompt([
         {
@@ -142,27 +159,11 @@ function addEmployee() {
             type: 'input',
             name: 'employeeRole',
             message: 'Please enter the employee role id.',
-            validate: employeeRoleInput => {
-                if (employeeRoleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the role!');
-                    return false;
-                }
-            }
         },
         {
             type: 'input',
             name: 'employeeManager',
             message: 'Please enter the employee manager.',
-            validate: employeeManagerInput => {
-                if (employeeManagerInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the manager!');
-                    return false;
-                }
-            }
         }
     ]).then((userInput) => {
         const sql = 'INSERT INTO employees (first_name, last_name, role_id, manager) VALUES (?,?,?,?)';
@@ -172,7 +173,7 @@ function addEmployee() {
             userInput.employeeRole,
             userInput.employeeManager]
        
-            connection.query(sql, employee, function (err, results) {
+            connection.query(sql, employee, function (err) {
                 if (err) throw (err);
                 console.table(employeesTable());
             }
@@ -180,13 +181,13 @@ function addEmployee() {
     })
 };
 
-//add role name, salary, department 
+//Add role to roles table 
 function addRole() {
     inquirer.prompt([
         {
             type: 'input',
             name: 'roleName',
-            message: 'Please enter the role you would like to add',
+            message: 'Please enter the role you would like to add.',
             validate: roleNameInput => {
                 if (roleNameInput) {
                     return true;
@@ -200,27 +201,11 @@ function addRole() {
             type: 'input',
             name: 'roleSalary',
             message: 'Please enter the salary for the role.',
-            validate: roleSalaryInput => {
-                if (roleSalaryInput) {
-                    return true;
-                } else {
-                    console.log('Please enter salary!');
-                    return false;
-                }
-            }
         },
         {
             type: 'input',
             name: 'roleDepartment',
             message: 'Please enter department id for this role',
-            validate: roleDepartmentInput => {
-                if (roleDepartmentInput) {
-                    return true;
-                } else {
-                    console.log('Please enter department!');
-                    return false;
-                }
-            }
         }
     ]).then((userInput) => {
         const sql = 'INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)';
@@ -229,7 +214,7 @@ function addRole() {
             userInput.roleSalary,
             userInput.roleDepartment]
        
-            connection.query(sql, role, function (err, results) {
+            connection.query(sql, role, function (err) {
                 if (err) throw (err);
                 console.table(rolesTable());
             }
@@ -237,8 +222,8 @@ function addRole() {
     })
 };
 
+//Update employee role in employees table
 function updateRole() {
-    //let employees = 'SELECT * FROM employees';
     inquirer.prompt([
         {
             type: 'input',
@@ -251,9 +236,58 @@ function updateRole() {
             message: 'What is the new role id?'
         }
     ]).then((userInput) => {
-        connection.query(`UPDATE employees SET role_id = ${userInput.updateRole} WHERE id = ${userInput.updateEmp}`, function(err, results){
+        connection.query(`UPDATE employees SET role_id = ${userInput.updateRole} WHERE id = ${userInput.updateEmp}`, function(err){
             if(err) throw err;
             console.table(employeesTable());
         });
     });
 };
+
+//Delete employee from employees table
+function deleteEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'deleteEmp',
+            message: 'What is the id of the emlpoyee you would like to delete?',
+        }
+    ]).then((userInput) => {
+        connection.query(`DELETE FROM employees WHERE id = ${userInput.deleteEmp}`, function(err){
+        if(err) throw err;
+        console.table(employeesTable());
+        });
+    });
+};
+
+//delete roles from roles table
+function deleteRoles() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'deleteRoles',
+            message: 'What is the id of the role you would like to delete?',
+        }
+    ]).then((userInput) => {
+        connection.query( `DELETE FROM roles WHERE id = ${userInput.deleteRoles}`, function(err) {
+            if(err) throw err;
+            console.table(rolesTable());
+        });
+    });
+};
+
+//delete department from departments table
+function deleteDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'deleteDep',
+            message: 'What is the id of the department you would like to delete?',
+        }
+    ]).then((userInput) => {
+        connection.query(`DELETE FROM departments WHERE id = ${userInput.deleteDep}`, function(err) {
+            if(err) throw err;
+            console.table(departmentTable());
+        })
+    })
+}
+
